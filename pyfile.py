65d8d905bf8832ce7644c420b2daf395ae4d179f
Bit <bitbytenetwork@gmail.com>
2017-03-10 18:34:21 -0700

    Update pyfile.py

--- pyfile.py
+++ pyfile.py
@@ -6,15 +6,20 @@ import shutil
 import time
 import signal
 import math
+import subprocess
+from operator import eq
+import stat
 
 width = 80
 height = 20
 terminalConfig = None
-cd = os.getcwd()
+cd = sys.argv[1] if len(sys.argv) >= 2 else os.getcwd()
 display = []
+paint = []
 columnLength = 20
 selected = 0
 keypressCache = []
+files = []
 
 def inputMode():
   global terminalConfig
@@ -30,18 +35,26 @@ def updateSize():
   global width, height
   width, height = size[0], size[1]
 
-def clear():
+def top():
   #time.sleep(2)
-  # sys.stdout.write("\r" + chr(27) + "[H")
-  sys.stdout.write("\r" + (chr(27) + "[A") * height)
+  # sys.stdout.write("\r" + chr(27) + "[H") # This wasn't working
+  sys.stdout.write("\r" + (chr(27) + "[A") * height) # Press the up key height times
   #time.sleep(2)
   # sys.stdout.write((((" " * width) + "\r\n") * height)[:-2]) # No need to clear the stdout... the display is already empty and does that
   # sys.stdout.write("\r" + chr(27) + "[H")
 
+def flushPaint():
+  global paint
+  updateSize()
+  paint = []
+  for index1d in range(height):
+    paint.append([])
+    for index2d in range(width):
+      paint[index1d].append(" ")
+
 def flushDisplay():
   global display
   updateSize()
-  clear()
   display = []
   for index1d in range(height):
     display.append([])
@@ -49,13 +62,26 @@ def flushDisplay():
       display[index1d].append(" ")
 
 def updateDisplay():
-  global display
-  for line in range(len(display)):
-    if line != len(display) - 1:
-      sys.stdout.write(("".join(display[line]) + "\r\n"))
+  global display, paint
+  top()
+  for line in range(len(paint)):
+    if paint[line] != display[line]:
+      for char in range(len(paint[line])):
+        updated = False
+        if paint[line][char] != display[line][char]:
+          sys.stdout.write(paint[line][char])
+          display[line][char] = paint[line][char]
+          updated = True
+        if char == len(paint[line]) - 1: # Last char in line, need a newline for next row
+          if line != len(paint) - 1:
+            sys.stdout.write(paint[line][char] + "\r\n\r")
+          else: # Last line in display, no new line, don't forget to flush
+            sys.stdout.write(paint[line][char])
+            sys.stdout.flush()
+        elif not updated:
+          sys.stdout.write(chr(27) + "[C") # Move caret ahead
     else:
-      sys.stdout.write("".join(display[line]))
-      sys.stdout.flush()
+      sys.stdout.write(chr(27) + "[B") # Move caret down
 
 def draw(givenList, text, position, direction):
   # Direction: 0: left to right, 1: right to left, 2: down to up, 3: up to down
@@ -97,57 +123,124 @@ def rgbColorTile(givenList, start, end, color, bgColor):
     for indexX in range(end[1] - start[1]):
       givenList[start[0] + indexY][start[1] + indexX] = rgbColor(givenList[start[0] + indexY][start[1] + indexX], color, bgColor)
 
-def redraw(checkInput):
-  global display, width, height, keypressCache
-  flushDisplay()
+def prompt(text, placeholder, promptColor, inputColor, bgColor):
+  global paint, height, width
+  draw(paint, " " * width, [-1, 0], 0) # Clear the status bar
+  if len(text) >= width - 10:
+    text = text[:width - 13] + ".."
+  draw(paint, text, [-1, 1], 0)
+  
+  inputText = placeholder
+  position = len(placeholder)
+  while True:
+    draw(paint, inputText, [-1, 3 + len(text)], 0)
+    rgbColorTile(paint, [-2, 1], [-1, 1 + len(text)], promptColor, bgColor) # Paint the prompt text
+    rgbColorTile(paint, [-2, 0], [-1, -1], inputColor, bgColor) # Paint the status bar
+    redraw(False, True)
+    # print(inputText)
+    key = ord(sys.stdin.read(1))
+    inputText += chr(key)
+    position += 1
+
+def redraw(checkInput, smart):
+  global display, width, height, keypressCache, files, selected
   if checkInput == True:
     key = ord(sys.stdin.read(1))
     keypressCache.append(key)
     keypressCache = keypressCache[-5:]
-    draw(display, str(keypressCache), [6, 90], 0)
-    keyHandle(key)
-  # begin draw code #
+    draw(paint, str(keypressCache), [0, 0], 0)
+    if keyHandle(key) == False:
+      return
+    
+  if smart == True:
+    # begin smart draw code #
+    size = str(height) + " x " + str(width) # Terminal size
+    draw(paint, size, [-2, -1 - len(size)], 0)
+    
+    files = os.listdir(cd)
+    for file in range(len(files)):
+      column = math.floor(file / (height - 3))
+      row = file - (column * (height - 3))
+      filename = files[file]
+      if len(filename) >= columnLength:
+        filename = filename[:columnLength - 3] + ".."
+      draw(paint, filename, [1 + row, 1 + column * columnLength], 0)
+      if file == selected:
+        rgbColorTile(paint, [1 + row, 1 + column * columnLength], [2 + row, column * columnLength + len(filename) + 1], [0x10, 0x10, 0x10], [0xA0, 0xA0, 0xA0])
+      else:
+        rgbColorTile(paint, [1 + row, 1 + column * columnLength], [2 + row, column * columnLength + len(filename) + 1], [0xA0, 0xA0, 0xA0], [0x10, 0x10, 0x10])
+        # end smart draw code #
+  else:
+    # begin draw code #
+    
+    # for line in range(len(display)): # Line numbers
+      # draw(display, str(line), [line, 0], 0)
   
-  # for line in range(len(display)): # Line numbers
-    # draw(display, str(line), [line, 0], 0)
-
-  size = str(height) + " x " + str(width) # Terminal size
-  draw(display, size, [-1, -1 - len(size)], 0)
+    size = str(height) + " x " + str(width) # Terminal size
+    draw(paint, size, [-2, -1 - len(size)], 0)
+    
+    files = os.listdir(cd)
+    for file in range(len(files)):
+      column = math.floor(file / (height - 3))
+      row = file - (column * (height - 3))
+      filename = files[file]
+      if len(filename) >= columnLength:
+        filename = filename[:columnLength - 3] + ".."
+      draw(paint, filename, [1 + row, 1 + column * columnLength], 0)
+      if file == selected:
+        rgbColorTile(paint, [1 + row, 1 + column * columnLength], [2 + row, column * columnLength + len(filename) + 1], [0x10, 0x10, 0x10], [0xA0, 0xA0, 0xA0])
+    
+    # colors
+    # rgbColorTile(paint, [0, -21], [-1, -20], [0x80, 0x80, 0x80], [0x5, 0x5, 0x5])
+    rgbColorTile(paint, [0, 0], [-2, -1], [0xA0, 0xA0, 0xA0], [0x10, 0x10, 0x10]) # Paint everything c#101010 bgc#A0A0A0
+    rgbColorTile(paint, [-2, 0], [-1, -1], [0x10, 0x10, 0x10], [0xA0, 0xA0, 0xA0]) # Paint the status bar
+    # end draw code #
   
-  dirList = os.listdir("/bin")
-  for file in range(len(dirList)):
-    column = math.floor(file / (height - 2))
-    row = file - (column * (height - 2))
-    filename = dirList[file]
-    if len(filename) >= columnLength:
-      filename = filename[:columnLength - 3] + ".."
-    draw(display, filename, [1 + row, 1 + column * columnLength], 0)
-    if file == selected:
-      rgbColorTile(display, [1 + row, 1 + column * columnLength], [2 + row, column * columnLength + columnLength], [0x10, 0x10, 0x10], [0xA0, 0xA0, 0xA0])
-  
-  # colors
-  rgbColorTile(display, [0, -21], [-1, -20], [0x80, 0x80, 0x80], [0x5, 0x5, 0x5])
-  rgbColorTile(display, [0, 0], [-1, -1], [0xA0, 0xA0, 0xA0], [0x10, 0x10, 0x10]) # Paint everything c#101010 bgc#A0A0A0
-  # end draw code #
   updateDisplay()
 
 def keyHandle(key): # 27 91 66 - 27 91 65
-  global keypressCache, selected
-  draw(display, str(key), [0, 2], 0)
+  global keypressCache, selected, paint, files, selected, cd
   if keypressCache[-3:] == [27, 91, 66]: # down key
     selected += 1
   elif keypressCache[-3:] == [27, 91, 65]: # up key
     selected -= 1
-  # time.sleep(3)
+  elif key == 127: # Backspace
+    cd += "/.."
+    selected = 0
+    flushPaint()
+    redraw(False, False)
+  elif keypressCache[-4:] == [27, 91, 51, 126]: # Delete key
+    prompt("Delete file?", "default text", [0xA0, 0xA0, 0xA0], [0x60, 0x60, 0x60], [0xff, 0x0, 0x0])
+  elif key == 10: # Enter key
+    target = cd + "/" + files[selected]
+    if os.path.isdir(target):
+      cd = target
+      selected = 0
+      flushPaint()
+      redraw(False, False)
+    elif stat.S_IXUSR & os.stat(target)[stat.ST_MODE]:
+      flushPaint()
+      updateDisplay()
+      resetMode()
+      os.system(target)
+      sys.stdout.write("Process finished. Press any key to continue...")
+      sys.stdout.flush()
+      inputMode()
+      sys.stdin.read(1)
+      redraw(False, False)
 
 def signalHandler(signal, frame):
+  exit(0)
+  
+def exit(code):
   resetMode()
-  sys.exit(1)
+  sys.exit(code)
 
 signal.signal(signal.SIGINT, signalHandler)
 
 inputMode()
-redraw(False)
+flushPaint()
+flushDisplay()
+redraw(False, False)
 while 1:
-  redraw(True)
-  # time.sleep(1)
+  redraw(True, True)
